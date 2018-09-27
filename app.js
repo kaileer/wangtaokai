4901c7659bbd6fd9aa9cf1c50928abf6f093f12b
王韬凯 <wangtaokai@smartcinema.com.cn>
2018-09-28 01:31:46 +0800

    server

---
+++ app.js
@@ -0,0 +1,22 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const static = require('koa-static')
+const { resolve } = require('path')
+
+
+app.use(views(
+    resolve(__dirname, './views'),
+    {
+        extension: 'ejs'
+    }
+))
+app.use(static(
+    resolve(__dirname, './public')
+))
+app.use(async (ctx,next) => {
+	await ctx.render('index', {
+        name: 'wangtaokai'
+    })
+})
+app.listen(8081)
